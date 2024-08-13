1b402bc21fd85d9dd646e2418b52dedb77f9015a
Steven Kikim <stevenkikim35@gmail.com>
2024-08-13 21:45:41 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xE48a7DB3aD8f0C4a3b78e9b89eCd3c9a39d7D63f'
+export const ProxyAdmin = '0x621C074a8661d2B62766f6DCc4E4587186023CA8'
+export const SWTRImplementation = '0x30e713782c65B45d5386756f9bDFf3879E105F70'
     
